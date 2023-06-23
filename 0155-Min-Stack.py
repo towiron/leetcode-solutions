5d65c59fa565c7af4a54cc9c0c02583d297c5330
towiron <towiron@towirons-MacBook-Air.local>
2023-06-23 14:04:50 +0500

    Add Python solution for 0155 Min Stack

---
+++ 0155-Min-Stack.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.stack_min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.stack_min:
+            val = min(val, self.stack_min[-1])
+        self.stack_min.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.stack_min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.stack_min[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
